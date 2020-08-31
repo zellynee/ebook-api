5f6d91cca73e5425d0a60e2522f945fc0ab46505
zellynee <zellyneoctavianii234@gmail.com>
2020-08-31 18:17:47 +0700

    challenge-author

--- app/Book.php
+++ app/Book.php
@@ -8,6 +8,5 @@ class Book extends Model
 {
     protected $fillable = ['title','description','author','publisher','date_of_issue'];
 
-    //nggonku mau kaya kue ternyata xampp urung nyala tapi nggonmu uwis padahal
-    //makane aku takon anu step e pa bae mbok an sing kurang koh:(((())))
+    //
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('books','BookController');
+Route::resource('authors','AuthorController');
 
